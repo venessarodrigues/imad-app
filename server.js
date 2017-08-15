77f8213efa14709aaf3027fe6a01ef3005506dbb
venessarodrigues <venessardrgs4@gmail.com>
2017-08-15 20:30:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ var htmltemplate=
     
 </html>`
 ;
-return htmltemplate
+return htmltemplate;
 }
 
 app.get('/', function (req, res) {
@@ -75,7 +75,11 @@ app.get('/:articleName',function (req,res) {
     var articleName=req.params.articleName;
     res.send(createtemplate(articles[articleName]));
 });
-
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
