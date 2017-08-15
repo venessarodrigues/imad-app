0d7766e2d08e44e67b5d43eadaf9196f17939b48
venessarodrigues <venessardrgs4@gmail.com>
2017-08-15 22:39:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ var names=[];
 app.get('/submit-name/:name', function (req, res) {
   var name=req.params.name;
   names.push(name);
-  res.send(JSON.Stringify(names));
+  res.send(JSON.stringify(names));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
