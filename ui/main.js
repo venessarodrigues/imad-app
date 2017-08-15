9933ff86ab085a1d636c4a1e4c0bc15ab6baf1ed
venessarodrigues <venessardrgs4@gmail.com>
2017-08-15 20:52:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 
 //change content of html 
@@ -18,4 +18,11 @@ img.onclick = function ()
     }
     var interval=setInterval(moveRight,100);
     
+};*/
+var counter=0;
+var button=document.getElementById('counter');
+button.onclick=function(){
+  counter=counter+1;
+  var count=document.getElementById('count');
+  count.innerHTML('counter');
 };
