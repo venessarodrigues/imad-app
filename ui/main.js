79343971644b2f266c39d615808c8f18bd477c16
venessarodrigues <venessardrgs4@gmail.com>
2017-08-15 19:58:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,12 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='hello';
+
+//move an image
+
+var img=document.getELementById('img');
+img.onclick = function()
+{
+    img.style.marginLeft='100px';
+    
+};
