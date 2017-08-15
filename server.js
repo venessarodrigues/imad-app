a3f540ffef12fba64c3e57298b563749cc8cc7b8
venessarodrigues <venessardrgs4@gmail.com>
2017-08-15 22:39:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var names=[];
 app.get('/submit-name/:name', function (req, res) {
   var name=req.params.name;
   names.push(name);
