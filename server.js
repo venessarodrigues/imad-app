6bcd1e07e9f3a77ec4e28907f5e7f831b964ae33
venessarodrigues <venessardrgs4@gmail.com>
2017-08-15 20:41:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,9 +71,9 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter=0;
-app.get('/counter',function (req,res) {
-    counter=counter+1;
+var counter = 0;
+app.get('/counter', function (req , res) {
+    counter = counter + 1;
     res.send(counter.toString());
 });
 
