e72a3a9400c05955c3a18e5e78b2c7533c1e6427
venessarodrigues <venessardrgs4@gmail.com>
2017-08-16 13:47:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config ={
     database:'venessardrgs4',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DBpassword
+    password:process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
