50af5523b7f351a96af4a7b239f1c42cedf8cf41
venessarodrigues <venessardrgs4@gmail.com>
2017-08-16 20:34:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').Pool;
-var crypto=require('crypto');
+var crypto = require('crypto');
 var config ={
     user:'venessardrgs4',
     database:'venessardrgs4',
@@ -125,7 +125,7 @@ app.get('/articles/:articleName',function (req,res) {
 });
 function hash(input,salt)
 {
-  var hashed= crypto.pbkdf25ync(input,salt,10000,512,'sha512');
+  var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
   return hashed.toString('hex');
 }
 
