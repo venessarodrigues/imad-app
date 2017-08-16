406bc6beab57d81bf6ed257c74eee407b4f06a0b
venessarodrigues <venessardrgs4@gmail.com>
2017-08-17 00:17:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,28 +19,6 @@ img.onclick = function ()
     var interval=setInterval(moveRight,100);
     
 };*/
-var button=document.getElementById('counter');
-button.onclick=function(){
-    
-    var request=new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {
-       if(request.readyState === XMLHttpRequest.DONE)
-       {
-           if(request.status === 200)
-           {
-               var counter=request.responseText;
-               var count=document.getElementById('count');
-               count.innerHTML=counter.toString();
-           }
-       }
-    };
-    
-    
-    
-    request.open('GET','http://venessardrgs4.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
 
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
@@ -52,23 +30,25 @@ submit.onclick=function()
        {
            if(request.status === 200)
            {
-                var names=request.responseText;
-                var names=JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++)
-                {
-                   list += '<li>' +names[i]+ '<l1>' ;
-                }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+               console.log('user looged in');
+               alert('logged successfully');
+           }
+           else if(request.status===403)
+           {
+               alert('invalid');
+           }
+           else if(request.status===500)
+           {
+               alert('something went wrong');
            }
        }
     };
     
-    var nameinput=document.getElementById('name');
-    var name=nameinput.value;
-    
-    request.open('GET','http://venessardrgs4.imad.hasura-app.io/submit-name?name=' +name,true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username,password);
+    request.setRequestHeader('content-type','application/json');
+    request.open('POST','http://venessardrgs4.imad.hasura-app.io/login',true);
+    request.send(JSON.stringify({username:username,password:password}));
 };
     
