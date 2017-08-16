ba45fcc5efbf555bc04739d97766113a985228a9
venessarodrigues <venessardrgs4@gmail.com>
2017-08-16 23:50:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.post('/login',function(req,res)
             {
                 var dbstring=result.rows[0].password;
                 var salt=dbstring.split('$')[2];
-                var hashed=hash(dbstring,salt);
+                var hashed=hash(password,salt);
                 if(hashed===password)
                 {
                   res.send('credentials valid');  
