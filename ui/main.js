7f7f90109dbd14455a917fa92b0bb749233704ae
venessarodrigues <venessardrgs4@gmail.com>
2017-08-17 00:29:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,9 +45,10 @@ submit.onclick=function()
     
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
-    console.log(username,password);
-    request.setRequestHeader('Content-Type','application/json');
+    console.log(username);
+    console.log(password);
     request.open('POST','http://venessardrgs4.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
 };
     
